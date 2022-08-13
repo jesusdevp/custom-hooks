732d45f9f8d5fb7f5fc694bc0c36dc0e79cb9aac
Jesus Ramirez <jesusdevelp@gmail.com>
2022-08-13 09:44:41 -0500

    todoReducer agregado

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,28 @@
+export const todoReducer = (initialState = [], action) => {
+
+    switch (action.type) {
+        case '[TODO] Add Todo':
+            return [...initialState, action.payload]
+
+        case '[TODO] Delete Todo':
+            return initialState.filter((todo) => todo.id !== action.payload)
+
+        case '[TODO] Done todo':
+            return initialState.map((todo) => {
+
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo
+            })
+
+        default:
+            return initialState
+    }
+
+
+}
